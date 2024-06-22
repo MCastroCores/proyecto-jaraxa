bb90c1dd9d702da1592036e32a3a67ed0f66f2a9
Martín Castro <castro.cores77@gmail.com>
2024-06-22 09:37:21 +0200

    added styles both pages

--- src/pages/DrugPage.jsx
+++ src/pages/DrugPage.jsx
@@ -1,14 +1,29 @@
 import { useParams } from "react-router-dom";
 import {
   Box,
-  Button,
   Card,
-  CardActions,
   CardContent,
   Grid,
+  ListItemButton,
+  ListItemText,
   Typography,
+  createTheme,
 } from "@mui/material";
+import CachedIcon from "@mui/icons-material/Cached";
 import { useEffect, useState } from "react";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#0C1B33",
+    },
+    text: {
+      primary: "#fff",
+      secondary: "#46505A",
+    },
+  },
+});
 
 export const DrugPage = () => {
   // Extraemos de los params el nombre para realizar otra petición a la API con el producto deseado
@@ -27,7 +42,7 @@ export const DrugPage = () => {
   console.log(URL);
 
   useEffect(() => {
-    const getResults = async () => {
+    const getResultsEspecific = async () => {
       try {
         const response = await fetch(URL);
         const dataAPI = await response.json();
@@ -37,50 +52,68 @@ export const DrugPage = () => {
       }
     };
 
-    getResults();
+    getResultsEspecific();
   }, []);
 
   console.log(dataDrug);
 
   return (
-    <Box
-      component="main"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Typography sx={{ width: 250 }} variant="h4" component="h1" padding={3}>
-        Proyect Jaraxa
-      </Typography>
-      {dataDrug?.results ? (
-        <Grid container spacing={2}>
-          {dataDrug.results.map((drug) => (
-            <Grid item xs={12} md={6} xl={4} key={drug.id}>
-              <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 14 }}
-                    color="text.secondary"
-                    gutterBottom
-                  >
-                    {drug.openfda.generic_name}
-                  </Typography>
-                  <Typography variant="h5" component="div">
-                    {drug.sponsor_name}
-                  </Typography>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      ) : (
-        <Box>No results</Box>
-      )}
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        component="main"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "background.paper",
+          color: "text.primary",
+          minHeight: "100vh",
+        }}
+      >
+        <Typography sx={{ width: 300 }} variant="h4" component="h1" padding={3}>
+          PROYECT JARAXA
+        </Typography>
+        {dataDrug?.results ? (
+          <Grid container spacing={2}>
+            {dataDrug.results.map((drug, index) => (
+              <Grid item xs={12} md={6} xl={4} key={index}>
+                <Card sx={{ minWidth: 300, borderColor: "text.primary" }}>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 28 }}
+                      color="text.primary"
+                      gutterBottom
+                    >
+                      {drug.openfda.generic_name}
+                    </Typography>
+                    {drug.products?.map((product, index) => (
+                      <ListItemButton key={index} variant="h5" component="div">
+                        <ListItemText>{product.brand_name}</ListItemText>
+                        <ListItemText>{product.dossage_form}</ListItemText>
+                        <ListItemText>{product.route}</ListItemText>
+                      </ListItemButton>
+                    ))}
+                    <Typography variant="h5" component="div">
+                      {drug.sponsor_name}
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      component="div"
+                      sx={{ fontSize: 12 }}
+                    >
+                      {drug.application_number}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Box sx={{ color: "text.primary", padding: 10 }}>
+            <CachedIcon />
+          </Box>
+        )}
+      </Box>
+    </ThemeProvider>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,45 +14,60 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import VaccinesIcon from "@mui/icons-material/Vaccines";
 import CachedIcon from "@mui/icons-material/Cached";
-import { ListItemText } from "@mui/material";
+import { ListItemText, createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#0C1B33",
+    },
+    text: {
+      primary: "#fff",
+      secondary: "#46505A",
+    },
+  },
+});
 
 export const HomePage = () => {
-  // ESTADOS PARA GUARDAR LA PALABRA A BUSCAR Y LA INFORMACIÓN DE LA API
+  // ESTADOS PARA GUARDAR LA PALABRA A BUSCAR Y ESTADO PARA GUARDAR LA INFORMACIÓN AGRUPADA DE LAS MEDICINAS DE LA API
   const [query, setQuery] = useState("");
-  const [data, setData] = useState(null);
+  const [dataDrugCounter, setDataDrugCounter] = useState(null);
 
   const URL = `https://api.fda.gov/drug/drugsfda.json?search=${query}&count=openfda.generic_name.exact&limit=500`;
-  // const URLPRUEBA = `https://api.fda.gov/drug/drugsfda.json?search=openfda.generic_name.exact:${query}&limit=500`;
 
   useEffect(() => {
-    const getResults = async () => {
+    const getResultsByGroup = async () => {
       try {
         const response = await fetch(URL);
         const dataAPI = await response.json();
-        setData(dataAPI);
+        setDataDrugCounter(dataAPI);
       } catch (error) {
         console.log(error);
       }
     };
 
-    getResults();
+    getResultsByGroup();
   }, [query, URL]);
 
   console.log(query);
-  console.log(data);
+  console.log(dataDrugCounter);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Box
         component="main"
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          backgroundColor: "background.paper",
+          color: "text.primary",
+          minHeight: "100vh",
         }}
       >
-        <Typography sx={{ width: 250 }} variant="h4" component="h1" padding={3}>
-          Proyect Jaraxa
+        <Typography sx={{ width: 300 }} variant="h4" component="h1" padding={3}>
+          PROYECT JARAXA
         </Typography>
         <Box
           component="form"
@@ -63,18 +78,20 @@ export const HomePage = () => {
           autoComplete="off"
         >
           <TextField
+            sx={{ borderColor: "text.primary" }}
             id="outlined"
             label="Search a drug..."
+            borderColor="white"
             onChange={(e) => {
               setQuery(e.target.value.toUpperCase());
             }}
           />
         </Box>
-        {data ? (
+        {dataDrugCounter ? (
           <Box sx={{ width: "75%" }}>
-            <List>
-              {data?.results ? (
-                data.results.map((drug, index) => (
+            <List sx={{ display: "flex", flexDirection: "column" }}>
+              {dataDrugCounter?.results ? (
+                dataDrugCounter.results.map((drug, index) => (
                   <Link
                     key={index}
                     className="linkDefault"
@@ -82,13 +99,20 @@ export const HomePage = () => {
                     color="inherit"
                     underline="none"
                   >
-                    <ListItemButton sx={{ border: 1, gap: 1, marginY: 1 }}>
-                      <ListItemIcon>
+                    <ListItemButton
+                      sx={{
+                        border: 1.5,
+                        gap: 0.5,
+                        marginY: 0.5,
+                        borderRadius: "6px",
+                        borderColor: "text.primary",
+                      }}
+                    >
+                      <ListItemIcon sx={{ color: "text.primary" }}>
                         <VaccinesIcon />
                       </ListItemIcon>
-
                       <ListItemText
-                        sx={{ textDecoration: "none" }}
+                        sx={{ textDecoration: "none", color: "text.primary" }}
                         primary={drug.term}
                         secondary={`${drug.count} products`}
                       />
@@ -103,11 +127,11 @@ export const HomePage = () => {
             </List>
           </Box>
         ) : (
-          <Box sx={{ color: "black", padding: 10 }}>
+          <Box sx={{ color: "text.primary", padding: 10 }}>
             <CachedIcon />
           </Box>
         )}
       </Box>
-    </>
+    </ThemeProvider>
   );
 };
